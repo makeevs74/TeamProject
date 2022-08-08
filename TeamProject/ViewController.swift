d10caca9916139f3ac792786cb9f42cec822bd33
makeevs74 <makeevs74@gmail.com>
2022-08-08 23:11:30 +0500

    Initial Commit

---
+++ TeamProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeamProject
+//
+//  Created by Sergey Makeev on 08.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
